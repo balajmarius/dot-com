de7cd9dd7bf414c492293beb0bab3d057f6fabbd
Marius Balaj <marius@Mariuss-Mac-mini.local>
2017-04-10 11:39:44 +0300

    moved html into php

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php header( 'Location: /index.html' ); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="icon" type="image/png" href="http://example.com/image.png" />
+  <title>Balaj Marius</title>
+  <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+  <div class="wrap">
+
+    <h1 class="title">Hi, I’m Marius Balaj.<br>You’ll find me:</h1>
+
+    <ul>
+      <li>Replying to you via <a href="mailto:balajmarius93@gmail.com" class="email">Email</a></li>
+      <li>Making calls with <a href="skype:marius.balaj" class="skype">Skype</a></li>
+      <li>Being an open-source pro on <a target="_blank" href="http://github.com/balajmarius" class="github">Github</a></li>
+      <li>Bustin’ pixels on <a target="_blank" href="http://dribbble.com/mariusbalaj" class="dribbble">Dribbble</a></li>
+    </ul>
+
+  </div>
+
+</body>
+
+</html>
