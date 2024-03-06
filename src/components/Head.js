c0c5be6225618dea7d63c41c2c4bceb6c7c86cd8
Balaj Marius <balajmarius@gmail.com>
2024-03-06 12:04:32 +0200

    feat: added favicon

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import seo from "../data/seo.json";
+import favicon from "../static/images/favicon.svg";
 
 export default () => (
   <>
@@ -11,6 +12,7 @@ export default () => (
     <meta name="twitter:url" content={seo.url} />
     <meta name="twitter:description" content={seo.description} />
     <meta name="twitter:creator" content={seo.twitter} />
+    <link rel="icon" href={favicon} />
     <title>{seo.title}</title>
     <body className="font-sans bg-black text-gray-50 text-xl md:text-2xl" />
   </>
