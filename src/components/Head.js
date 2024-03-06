eedad8265ba20bf0e739b7c528868044db4adf26
Balaj Marius <balajmarius@gmail.com>
2024-03-06 15:42:09 +0200

    chore: added SEO and social metatags

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,16 +2,22 @@ import React from "react";
 
 import seo from "../data/seo.json";
 import favicon from "../static/images/favicon.svg";
+import card from "../static/images/card.svg";
 
 export default () => (
   <>
     <html lang="en" />
     <meta name="description" content={seo.description} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={seo.title} />
-    <meta name="twitter:url" content={seo.url} />
-    <meta name="twitter:description" content={seo.description} />
-    <meta name="twitter:creator" content={seo.twitter} />
+    <meta property="og:title" content={seo.title} />
+    <meta property="og:url" content={seo.url} />
+    <meta property="og:description" content={seo.description} />
+    <meta property="og:image" content={card} />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:title" content={seo.title} />
+    <meta property="twitter:url" content={seo.url} />
+    <meta property="twitter:description" content={seo.description} />
+    <meta property="twitter:creator" content={seo.twitter} />
+    <meta property="twitter:image" content={card} />
     <link rel="icon" href={favicon} />
     <title>{seo.title}</title>
     <body className="font-sans bg-black text-gray-50 text-xl md:text-2xl selection:bg-white selection:text-black" />
