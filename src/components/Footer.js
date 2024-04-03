fc3de737f3353d5e27023be1d471100eec42aea6
Balaj Marius <balajmarius@gmail.com>
2024-04-03 16:29:14 +0300

    chore: remove underline

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default () => (
   <footer>
     <ul className="flex space-x-4">
       {socials.map(({ href, text }) => (
-        <a href={href} key={href} target="_blank" rel="noreferrer" className="text-gray-100 underline hover:text-white">
+        <a href={href} key={href} target="_blank" rel="noreferrer" className="text-gray-100 hover:text-white">
           {text}
         </a>
       ))}
