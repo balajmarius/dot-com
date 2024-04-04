153bbdedc7e35b09930b04aadab68a95335d6b48
Balaj Marius <balajmarius@gmail.com>
2024-04-04 09:15:45 +0300

    fix: mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import socials from "../data/socials.json";
 
 export default () => (
   <footer>
-    <ul className="flex space-x-4">
+    <ul className="flex flex-col md:flex-row space-y-1 md:space-y-0 md:space-x-4">
       {socials.map(({ href, text }) => (
         <a href={href} key={href} target="_blank" rel="noreferrer" className="text-gray-100 hover:text-white">
           {text}
