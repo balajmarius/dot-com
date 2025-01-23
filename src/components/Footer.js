4195fdcbb586758e693b1eadccae4d1beb03ea7e
Balaj Marius <balajmarius@gmail.com>
2025-01-23 18:01:20 +0200

    fix: color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default () => (
     <ul className="flex flex-col md:flex-row space-y-1 md:space-y-0 md:space-x-4">
       {socials.map(({ href, text, strike }) => (
         <li key={href} className={twMerge(strike && "line-through")}>
-          <a href={href} target="_blank" rel="noreferrer" className="text-gray-100 hover:text-white">
+          <a href={href} target="_blank" rel="noreferrer" className="text-gray-150 hover:text-white">
             {text}
           </a>
         </li>
